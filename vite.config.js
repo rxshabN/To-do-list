e409f495bbda47e66ca19c390155e5b3f3953bd4
rxshabN <nagwanirishab@gmail.com>
2025-02-02 00:30:30 +0530

    revert to tailwind v3'

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: "/To-do-list/",
 });
